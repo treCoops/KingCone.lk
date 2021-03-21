0498af4b275ed2ede3b354da0326e48bb2a4c044
treCoops <trevogayan1995@gmail.com>
2021-03-21 13:12:25 +0530

    Static Updates are Done

--- application/views/Template/scripts.php
+++ application/views/Template/scripts.php
@@ -4,3 +4,5 @@
 <script type="text/javascript" src="<?php base_url() ?>/assets/js/isotope.pkgd.min.js"></script>
 <script type="text/javascript" src="<?php base_url() ?>/assets/js/jquery.circliful.min.js"></script>
 <script type="text/javascript" src="<?php base_url() ?>/assets/js/global.js"></script>
+<script type="text/javascript" src="<?php base_url() ?>/assets/js/js.js"></script>
+<script src="<?php base_url() ?>/assets/js/map.js"></script>
--- application/views/contact.php
+++ application/views/contact.php
@@ -19,14 +19,14 @@
         </div>
     </div>
 </div>
-<div class="bgWhite">
+<div class="bgBlack">
     <div class="container">
-        <div class="emptySpace-xs60 emptySpace130"></div>
+        <div class="emptySpace-xs60 emptySpace130 backgroundBlack"></div>
         <div class="row">
             <!--  Contacts-1 -->
             <div class="col-sm-4">
                 <div class="big contactShortcode">
-                    <a class="contacts spin spin2"> <img src="<?php echo base_url() ?>/assets/img/contacts-icon.png" alt></a>
+                    <a class="contacts"> <img src="<?php echo base_url() ?>/assets/img/contacts-icon.png" alt></a>
                     <div class="emptySpace15"></div>
                     <p>244 5th Avenue. Suite 1735i</p>
                     <p>New York, N.Y.</p>
@@ -37,7 +37,7 @@
             <div class="col-sm-4">
                 <div class="emptySpace-xs30"></div>
                 <div class="big contactShortcode">
-                    <a class="contacts spin spin2"><img src="<?php echo base_url() ?>/assets/img/contacts-icon2.png" alt> </a>
+                    <a class="contacts"><img src="<?php echo base_url() ?>/assets/img/contacts-icon2.png" alt> </a>
                     <div class="emptySpace20"></div>
                     <a class="contactPhone" href="tel:5557858965">555. 785.8965</a>
                     <a class="contactPhone" href="tel:5558974321">555. 897.4321</a>
@@ -47,65 +47,60 @@
             <div class="col-sm-4">
                 <div class="emptySpace-xs30"></div>
                 <div class="big contactShortcode">
-                    <a class="contacts spin spin2"> <img src="<?php echo base_url() ?>/assets/img/contacts-icon3.png" alt></a>
+                    <a class="contacts"> <img src="<?php echo base_url() ?>/assets/img/contacts-icon3.png" alt></a>
                     <div class="emptySpace15"></div>
                     <a class="contactPhone" href="mailto:office@invisio.com">office@invisio.com</a>
                     <a class="contactPhone" href="mailto:cakes@invisio.com">cakes@invisio.com</a>
                 </div>
             </div>
         </div>
-        <div class="emptySpace-xs50 emptySpace105"></div>
-        <div class="row">
-            <div class="col-sm-6 col-sm-offset-3">
+<!--        <div class="emptySpace-xs50 emptySpace105"></div>-->
+<!--        <div class="row">-->
+<!--            <div class="col-sm-6 col-sm-offset-3">-->
                 <!-- Contacts-Title -->
-                <div class="big titleShortcode3">
-                    <h2 class="h2">Got Some Questions?</h2>
-                    <div class="emptySpace20"></div>
-                    <p>Please, feel yourself comfortable to fill in our contact form</p>
-                </div>
-            </div>
-        </div>
-        <div class="emptySpace-xs40 emptySpace85"></div>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <form onsubmit="return submitForm();" action="./" method="post" name="contactform" id="contact-form">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <input class="contactsInput" type="text" required name="name" placeholder="Full Name*">
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="emptySpace-xs20"></div>
-                            <input class="contactsInput" type="email" required name="email" placeholder="Email*">
-                        </div>
-                    </div>
-                    <div class="emptySpace20"></div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <input class="contactsInput" type="text" required name="subject" placeholder="Subject">
-                        </div>
-                    </div>
-                    <div class="emptySpace20"></div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <textarea class="contactsInput textArea" required name="message" cols="30" rows="5" placeholder="Message..."></textarea>
-                        </div>
-                    </div>
-                    <div class="emptySpace30"></div>
-                    <div class="btnAlign">
-                        <button class="button style-2 size-2" type="submit">Submit Message</button>
-                    </div>
-                    <p class="requestSuccess normall"></p>
-                </form>
-            </div>
-        </div>
+<!--                <div class="big titleShortcode3">-->
+<!--                    <h2 class="h2">Got Some Questions?</h2>-->
+<!--                    <div class="emptySpace20"></div>-->
+<!--                    <p>Please, feel yourself comfortable to fill in our contact form</p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="emptySpace-xs40 emptySpace85"></div>-->
+<!--        <div class="row">-->
+<!--            <div class="col-md-8 col-md-offset-2">-->
+<!--                <form onsubmit="return submitForm();" action="./" method="post" name="contactform" id="contact-form">-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-sm-6">-->
+<!--                            <input class="contactsInput" type="text" required name="name" placeholder="Full Name*">-->
+<!--                        </div>-->
+<!--                        <div class="col-sm-6">-->
+<!--                            <div class="emptySpace-xs20"></div>-->
+<!--                            <input class="contactsInput" type="email" required name="email" placeholder="Email*">-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="emptySpace20"></div>-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-sm-12">-->
+<!--                            <input class="contactsInput" type="text" required name="subject" placeholder="Subject">-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="emptySpace20"></div>-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-sm-12">-->
+<!--                            <textarea class="contactsInput textArea" required name="message" cols="30" rows="5" placeholder="Message..."></textarea>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="emptySpace30"></div>-->
+<!--                    <div class="btnAlign">-->
+<!--                        <button class="button style-2 size-2" type="submit">Submit Message</button>-->
+<!--                    </div>-->
+<!--                    <p class="requestSuccess normall"></p>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--        </div>-->
 
     </div>
 </div>
-<div class="emptySpace-xs60 emptySpace130"></div>
-
-<!-- Map -->
-<div id="map-canvas" data-lat="34.0151244" data-lng="-118.4729871" data-zoom="15"></div>
-<div class="addresses-block">
-    <a class="marker" data-lat="34.0151244" data-lng="-118.4729871" data-string="1. Here is some address or email or phone or something else..."></a>
-</div>
-<div class="emptySpace-xs30 emptySpace50"></div>
+<div class="emptySpace-xs60 emptySpace130 backgroundBlack"></div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.723105465497!2d79.94857431496995!3d6.923666294997446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2570d12d971a7%3A0x69beba2b7e2e786d!2sKing%20Cone%20Ice%20Cream%C2%B2!5e0!3m2!1sen!2slk!4v1616311913585!5m2!1sen!2slk" width="1500" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<div class="emptySpace-xs30 emptySpace50 backgroundBlack"></div>
